e7c4a5d0be277bade5f34d3dc6f38530b0f3d53d
sam higton <contact@samhigton.com>
2012-09-13 20:53:36 +0100

    adding project structure

---
+++ js/models/grid.js
@@ -0,0 +1,12 @@
+function grid (name,width,height,target) {
+	this.name = name;
+	this.width = width;
+	this.height = height;
+	this.target = target;
+	this.gridLayout = Array();
+	
+	this.buildGrid = function () {
+		rerer
+	}
+	
+}
---
+++ js/models/tileholder.js
@@ -0,0 +1,6 @@
+function tileHolder (name,xpos,ypos) {
+	this.Name = name;
+	this.Content = null;
+	this.xPos = xpos;
+	this.yPos = ypos;
+}
